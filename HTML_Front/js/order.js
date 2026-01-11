1b83c9785294d4472dddaf5be0b8a958dee209fe
Benedikt Erb <130462282+DieErbse2@users.noreply.github.com>
2026-01-11 13:08:38 +0100

    Update order.js. Ref #164
    Anpassung für korrekte befüllung uznd anzeigen von erfolg und fehlschlag

--- HTML_Front/js/order.js
+++ HTML_Front/js/order.js
@@ -1,49 +1,31 @@
-const LAMBDA_URL = "https://h6a15phvcf.execute-api.eu-north-1.amazonaws.com/buy"; // API-Gateway Url
+const LAMBDA_URL = "https://h6a15phvcf.execute-api.eu-north-1.amazonaws.com/buy";
 
 export async function orderSnack(product_id) {
     if (!product_id) {
         alert("Ungültiges Produkt");
         return;
     }
+
     try {
         const response = await fetch(LAMBDA_URL, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                produkt_id: product_id
-            })
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ produkt_id: product_id })
         });
 
         if (!response.ok) {
-            throw new Error("Fehler beim Aufruf der Lambda-Funktion");
+            localStorage.setItem("orderStatus", "error");
+            window.location.href = "order.html";
+            return;
         }
 
-        const result = await response.json();
-        var text = document.getElementById("orderText");
-        text.innerHTML= `
-            <span class="success">
-                Kauf erfolgreich.<br>
-                Bitte entnehmen sie ihr Produkt<br><br>
-            </span>
-        `;
+        localStorage.setItem("orderStatus", "success");
         window.location.href = "order.html";
-        setTimeout(() => {
-            window.location.href = "snackMaschine.html";
-        }, 10000);
+
     } catch (error) {
-        var text = document.getElementById("orderText");
-        text.innerHTML= `
-            <span class="error">
-                Kauf fehlgeschlagen.<br>
-                Der Gewählte Produkttyp ist nicht mehr vorhanden<br><br>
-            </span>
-        `;
-        window.location.href = "order.html";
         console.error("Fehler:", error);
-        setTimeout(() => {
-            window.location.href = "snackMaschine.html";
-        }, 10000);
+        localStorage.setItem("orderStatus", "error");
+        window.location.href = "order.html";
     }
 }
+
