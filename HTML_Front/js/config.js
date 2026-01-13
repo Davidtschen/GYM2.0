653476df1d609e45068ac30b2eeb86fd5e885ca2
Benedikt Erb <130462282+DieErbse2@users.noreply.github.com>
2026-01-13 10:41:07 +0100

    Erweiterung um Region. Ref#153

--- HTML_Front/js/config.js
+++ HTML_Front/js/config.js
@@ -1,4 +1,5 @@
 export const cognitoConfig = {
+    Region: "eu-north-1",
     authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_anlv1kQRJ",
     client_id: "64fqsf4bha6al02hpav3pu53vl",
     redirect_uri: "https://amplifyv2.d2r89bauojj5mo.amplifyapp.com/callback.html",
